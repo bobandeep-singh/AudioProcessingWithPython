1488cc7f2214489a4f1e2e65411888d3b4778c5c
Bobandeep Singh <bobandeepsingh@gmail.com>
2019-06-17 15:40:36 +0530

    Script for audio to text

---
+++ SpeechRecogGoogleApi.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+# use the audio file as the audio source
+AUDIO_FILE = 'path to wav file'
+
+r = sr.Recognizer()
+
+with sr.AudioFile(AUDIO_FILE) as source:
+	#reads the audio file. Here we use record instead of
+	#listen
+	audio = r.record(source)
+
+try:
+	print("The audio file contains: " + r.recognize_google(audio))
+
+except sr.UnknownValueError:
+	print("Google Speech Recognition could not understand audio")
+
+except sr.RequestError as e:
+	print("Could not request results from Google Speech Recognition service; {0}".format(e))
